34ad8180e71fad02418c74949da7224033e5a3b6
AshokProject <ashok.kumarabs77@gmail.com>
2026-02-17 00:56:34 -0500

    Added Extent Report

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -1,9 +1,16 @@
 package hooks;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import io.cucumber.java.Before;
 import io.cucumber.java.After;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import io.cucumber.java.Scenario;
+
+import java.io.File;
+import java.io.IOException;
 
 
 public class Hooks {
@@ -20,7 +27,37 @@ public class Hooks {
     }
 
     @After
-    public void tearDown() {
-        driver.quit();
+    public void tearDown(Scenario scenario) throws IOException {
+
+        if (driver != null) {
+            String status = scenario.getStatus().name(); // PASSED/FAILED
+            status = status.substring(0,1).toUpperCase() + status.substring(1).toLowerCase();
+
+            try {
+                // 1️⃣ Take screenshot as File
+                File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+                // 2️⃣ Define path
+                String screenshotPath = System.getProperty("user.dir")
+                        + "/test-output/screenshots/"
+                        + scenario.getName().replaceAll(" ", "_") + "_" + status + ".png";
+
+                File destFile = new File(screenshotPath);
+
+                // 3️⃣ Copy to destination
+                FileUtils.copyFile(srcFile, destFile);
+
+                System.out.println("Screenshot saved at: " + screenshotPath);
+
+                // 4️⃣ Attach screenshot for Extent using path (important)
+                scenario.attach(FileUtils.readFileToByteArray(destFile), "image/png", status + "_Screenshot");
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            driver.quit();
+        }
+
     }
 }
--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"stepdefinitions", "hooks"},
-        plugin = {"pretty", "html:target/cucumber-reports.html"}
+        plugin = {"pretty", "json:target/cucumber.json", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
